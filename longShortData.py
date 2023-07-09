224cf346fce52f935a7c1c9657e19b6a3811b8e9
Mohamed A.M <Mohamed.m365@outlook.com>
2023-07-09 11:47:04 +0100

    Finished the Long v Short Data code

--- longShortData.py
+++ longShortData.py
@@ -1,19 +1,54 @@
+import datetime
+import time
 import requests
 import json
 
+# Get current time and calculate timestamp for 24 hours ago
+current_time = int(time.time())
+twenty_four_hours_ago = current_time - 24*60*60
+
+# Define symbols and API parameters
+symbols = ["BTCUSDT_PERP.A", "ETHUSDT_PEPR.A", "BNBUSDT_PERP.A", "XRPUSDT_PERP.A", "DOGEUSDT_PERP.A",
+           "SOLUSDT_PERP.A", "LTCUSDT_PERP.A", "MATICUSDT_PERP.A", "AVAXUSDT_PERP.A", "BCHUSDT_PERP.A",
+           "LINKUSDT_PERP.A", "ATOMUSDT_PERP.A", "LDOUSDT_PERP.A", "APTUSDT_PERP.A", "ARBUSDT_PERP.A",
+           "OPUSDT_PERP.A", "MKRUSDT_PERP.A", "SNXUSDT_PERP.A", "AAVEUSDT_PERP.A", "NEARUSDT_PERP.A"]
+
 params = {
-    "symbols": "BTCUSDT_PERP.A",
+    "symbols": symbols[2],  # Change the index as needed to select the desired symbol
     "interval": "4hour",
-    "from": 1678358400,  # UNIX timestamp for July 8, 2023, 00:00:00 UTC
-    "to": 1678444799,  # UNIX timestamp for July 8, 2023, 23:59:59 UTC
+    "from": twenty_four_hours_ago,
+    "to": current_time,
     "api_key": "461e8931-6fc3-44a0-ad4f-ccb907ee0080"
 }
 
 url = "https://api.coinalyze.net/v1/long-short-ratio-history"
 
-response = requests.get(url, params=params)
+try:
+    # Make the API request and handle potential errors
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # Raise an exception for non-2xx status codes
 
-# Handle the response data as needed
-data = response.json()
+    # Parse the response JSON data
+    data = response.json()
 
-print(json.dumps(data, indent=2))
+    formatted_data = []
+
+    # Process the data and format the timestamp
+    for entry in data[0].get('history', []):
+        timestamp = entry.get('t')
+        if timestamp is not None:
+            formatted_timestamp = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+            formatted_entry = {
+                'symbol': params['symbols'],
+                'timestamp': formatted_timestamp,
+                'ratio': entry.get('r'),
+                'long_percentage': entry.get('l'),
+                'short_percentage': entry.get('s')
+            }
+            formatted_data.append(formatted_entry)
+
+    # Print the formatted data
+    print(json.dumps(formatted_data, indent=2))
+
+except requests.exceptions.RequestException as e:
+    print("Error making the API request:", str(e))
